1489a25542080081e3b47201e337f2e77890e11e
Treysen-Zobell <36913811+Treysen-Zobell@users.noreply.github.com>
2024-04-18 05:09:12 -0600

    Added files to git

---
+++ common/context.py
@@ -0,0 +1,13 @@
+
+def user_context(request):
+    """
+    Adds user group information to context to visibility of menus on page.
+    :param request: HTTP Request
+    :return: Additional context information
+    """
+    ctx = {}
+    if request.user.groups.filter(name='admin').exists():
+        ctx['is_admin'] = True
+    if request.user.groups.filter(name='customer').exists():
+        ctx['is_customer'] = True
+    return ctx
