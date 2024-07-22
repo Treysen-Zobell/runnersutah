7c9ad78eef35d9ed67512af1f4fae70bfcba7aeb
Treysen-Zobell <36913811+Treysen-Zobell@users.noreply.github.com>
2024-07-21 22:45:53 -0600

    Fixed CORS on production

--- runnersutah/settings.py
+++ runnersutah/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,15 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://runnersutah.com",
+]
+
+CSRF_ALLOWED_ORIGINS = [
+    "https://runnersutah.com",
 ]
 
 ROOT_URLCONF = "runnersutah.urls"
