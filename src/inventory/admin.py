49fe7de99c5fb8aa765b74c5698e3c041dcf7c97
treysenzobell <treysenzobell@gmail.com>
2025-12-04 19:59:23 -0700

    Registered storage locations with admin panel.

--- src/inventory/admin.py
+++ src/inventory/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
 from inventory.models import (
+    StorageLocation,
     InventoryChangeTemplate,
     InventoryChangeTemplateField,
     InventoryChange,
     InventoryChangeFieldValue,
 )
 
+admin.site.register(StorageLocation)
 admin.site.register(InventoryChangeTemplate)
 admin.site.register(InventoryChangeTemplateField)
 admin.site.register(InventoryChange)
